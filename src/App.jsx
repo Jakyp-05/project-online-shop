a5a2cb4d25db1bf5f16bd99fdf297cf4cb6e6613
Jakyp-05 <jakypjumagulov080@gmail.com>
2024-02-26 19:18:11 +0600

    basic sass

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Layout from './components/layout/Layout'
+
+import {Home} from './pages/index'
 
 function App() {
-  return (
+  return ( 
     <div>
-      Hello World
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route path='/' element={<Home />}/>
+        </Route>
+      </Routes>
     </div>
   )
 }
